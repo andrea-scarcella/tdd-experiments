b1a4fd8aaaedba81288460973cac6091e6a63aff
Andrea Scarcella <andrea@acolussi-PC.advansys.dom>
2013-08-25 13:45:46 +0200

    Added ContactFactory

---
+++ ContactManager/ContactManager.Tests/ContactFactoryTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ContactManager;
+
+namespace ContactManager.Tests
+{
+    [TestFixture]
+    public class ContactFactoryTests
+    {
+        [Test]
+        public void canCreateContactObject()
+        {
+            IContactFactory cf = new ContactFactory();
+            Contact c=cf.createContact();
+            Assert.IsNotNull(c);
+            Assert.IsInstanceOf<Contact>(c);
+
+        }
+        
+
+    }
+}
---
+++ ContactManager/ContactManager/ContactFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ContactManager
+{
+    public class ContactFactory : IContactFactory
+    {
+        public Contact createContact()
+        {
+            Contact c = new Contact();
+            return c;
+        }
+    }
+}
